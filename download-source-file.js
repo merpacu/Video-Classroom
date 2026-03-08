e36f8e3aaa3844d5cbf83e0eee6e15994024952b
merpacu <mpalcue765@g.educaand.es>
2026-03-08 11:53:54 +0100

    Add files via upload

---
+++ download-source-file.js
@@ -0,0 +1,11 @@
+/**
+ * download-source-file iDevice
+ *
+ * This iDevice displays download link for the source .elpx file.
+ * The actual download functionality is provided by exe_elpx_download.js
+ * which is included automatically when this iDevice is detected.
+ */
+(function() {
+    'use strict';
+    // No initialization needed - download handled by exe_elpx_download.js
+})();
